fc0ae431bca5941519f908e5af32e38c1030f2dd
Cesurhan <ces-ur@hotmail.com>
2017-04-20 13:19:22 +0200

    test/Refactor Profile Spec

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Profile, type: :model do
   describe "validations" do
     it "is invalid without a first_name" do
-      user = Profile.new(first_name: "")
-      user.valid?
-      expect(user.errors).to have_key(:first_name)
+      profile = Profile.new(first_name: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:first_name)
     end
 
     it "is invalid without a last_name" do
-      user = Profile.new(last_name: "")
-      user.valid?
-      expect(user.errors).to have_key(:last_name)
+      profile = Profile.new(last_name: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:last_name)
     end
 
     it "photo is set by default" do
