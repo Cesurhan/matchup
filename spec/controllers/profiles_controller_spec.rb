3e5ad91b17a95da8e70e4b7649b0c735c5dc3c94
Cesurhan <ces-ur@hotmail.com>
2017-04-20 17:40:54 +0200

    refactor/Pages and Profiles Tests

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -5,28 +5,28 @@ RSpec.describe ProfilesController, type: :controller do
   describe "GET #new" do
     it "returns http Found" do
       get :new
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET #create" do
     it "returns http Found" do
       get :new
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET #edit" do
     it "returns http Found" do
       get :new
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET #update" do
     it "returns http Found" do
       get :new
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(:found)
     end
   end
 end
