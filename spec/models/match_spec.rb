f90a0710b82e7b8bb777d59a82e02d25acb7b425
Cesurhan <ces-ur@hotmail.com>
2017-04-24 21:14:11 +0200

    refactor/test

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -32,4 +32,39 @@ RSpec.describe Match, type: :model do
       expect(match.user_id).to eq(student2.id)
     end
   end
+
+  describe "generate_matches" do
+    let!(:student1) { create :user }
+    let!(:student2) { create :user }
+    let!(:student3) { create :user }
+    let!(:student4) { create :user }
+
+    it "creates new matches" do
+      expect{Match.generate_matches(Date.today)}.to change(Match, :count).by(2)
+    end
+  end
+
+  describe "shuffle_student_ids" do
+
+    it "shuffles the user ids in an array" do
+      expect(Match.shuffle_student_ids).to_not eq(Match.student_ids)
+    end
+  end
+
+  describe "student_ids" do
+
+    it "has an array of all user ids" do
+      expect(Match.student_ids.size).to eq(4)
+    end
+  end
+
+  describe "delete_matches" do
+    let(:student1) { create :user }
+    let(:student2) { create :user }
+    let!(:match)   { create :match, match_id: student1.id, user_id: student2.id}
+
+    it "destroys matches" do
+      expect{Match.delete_matches(Date.today)}.to change(Match, :count).by(-1)
+    end
+  end
 end
